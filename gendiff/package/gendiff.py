1b40f7ee6b03180b2f84b3a8c9045d2b080dbaa3
pmerkushkin <pmerkushkin@tradingview.com>
2026-01-03 23:25:48 +0400

    pmerkushkin: add 2 commit

--- gendiff/package/gendiff.py
+++ gendiff/package/gendiff.py
@@ -1,9 +1,10 @@
 import argparse
 
 def main():
-    gendiff = argparse.ArgumentParser(prog='gendiff', description='Compares two configuration files and shows a difference.',usage='first_file second_file')
+    gendiff = argparse.ArgumentParser(prog='gendiff', description='Compares two configuration files and shows a difference.',usage='first_file second_file', epilog='set format of output')
     gendiff.add_argument('first_file')
     gendiff.add_argument('second_file')
+    gendiff.add_argument('-f', '--format')
     gendiff.print_help()
 
 if __name__ == '__main__':
